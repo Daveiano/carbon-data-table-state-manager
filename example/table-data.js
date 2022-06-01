2c2b46571a812b1227aa13fc837fed63f66d38ca
David Baetge <david.baetge@gmail.com>
2022-06-02 01:41:37 +0200

    Added rollup. Added readme. Updated package json.

--- example/table-data.js
+++ example/table-data.js
@@ -1,62 +1,62 @@
-import { TABLE_SORT_DIRECTION } from '../src/misc';
+import { TABLE_SORT_DIRECTION } from "../src/misc";
 
 export const columns = [
   {
-    id: 'name',
-    title: 'Name',
-    sortCycle: 'bi-states-from-ascending',
+    id: "name",
+    title: "Name",
+    sortCycle: "bi-states-from-ascending",
   },
   {
-    id: 'protocol',
-    title: 'Protocol',
+    id: "protocol",
+    title: "Protocol",
   },
   {
-    id: 'port',
-    title: 'Port',
-    sortCycle: 'tri-states-from-ascending',
+    id: "port",
+    title: "Port",
+    sortCycle: "tri-states-from-ascending",
   },
   {
-    id: 'rule',
-    title: 'Rule',
+    id: "rule",
+    title: "Rule",
   },
   {
-    id: 'attachedGroups',
-    title: 'Attached groups',
+    id: "attachedGroups",
+    title: "Attached groups",
   },
   {
-    id: 'status',
-    title: 'Status',
+    id: "status",
+    title: "Status",
   },
 ];
 
 export const rows = [
   {
     id: 1,
-    name: 'Load Balancer 1',
-    protocol: 'HTTP',
+    name: "Load Balancer 1",
+    protocol: "HTTP",
     port: 80,
-    rule: 'Round Robin',
+    rule: "Round Robin",
     attachedGroups: "Maureen's VM Groups",
-    status: 'Active',
+    status: "Active",
   },
   {
     id: 2,
-    name: 'Load Balancer 2',
-    protocol: 'HTTPS',
+    name: "Load Balancer 2",
+    protocol: "HTTPS",
     port: 443,
-    rule: 'Round Robin',
+    rule: "Round Robin",
     attachedGroups: "Maureen's VM Groups",
-    status: 'Active',
+    status: "Active",
   },
   {
     id: 3,
     selected: true,
-    name: 'Load Balancer 3',
-    protocol: 'HTTP',
+    name: "Load Balancer 3",
+    protocol: "HTTP",
     port: 80,
-    rule: 'Round Robin',
+    rule: "Round Robin",
     attachedGroups: "Maureen's VM Groups",
-    status: 'Active',
+    status: "Active",
   },
 ];
 
@@ -65,12 +65,12 @@ export const rowsMany = Array.from(new Array(50))
     rows.map((row, j) => ({
       ...row,
       id: i * 3 + j,
-      name: `Load Balancer ${String(i * 3 + j + 1).padStart(3, '0')}`,
+      name: `Load Balancer ${String(i * 3 + j + 1).padStart(3, "0")}`,
     }))
   )
   .flat();
 
 export const sortInfo = {
-  columnId: 'name',
+  columnId: "name",
   direction: TABLE_SORT_DIRECTION.ASCENDING,
 };
---
+++ rollup.config.js
@@ -0,0 +1,29 @@
+import sass from "rollup-plugin-sass";
+import typescript from "rollup-plugin-typescript2";
+import external from "rollup-plugin-peer-deps-external";
+
+import pkg from "./package.json";
+
+export default {
+  input: "src/index.tsx",
+  output: [
+    {
+      file: pkg.main,
+      format: "cjs",
+      exports: "named",
+      sourcemap: true,
+    },
+    {
+      file: pkg.module,
+      format: "es",
+      exports: "named",
+      sourcemap: true,
+    },
+  ],
+  plugins: [
+    external(),
+    sass({ insert: true }),
+    typescript({ rollupCommonJSResolveHack: true }),
+  ],
+  external: ["react", "react-dom"],
+};
